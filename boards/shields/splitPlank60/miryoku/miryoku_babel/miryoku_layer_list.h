183782ea736c1e41487e2ad6d4ff2e7330543a49
ManosMpampis <manosbampis@gmail.com>
2025-03-22 17:55:35 +0200

    add new front row keys

--- boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_list.h
+++ boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_list.h
@@ -9,7 +9,6 @@
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
 MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(GAME,  "Game") \
 MIRYOKU_X(TAP,    "Tap") \
 MIRYOKU_X(BUTTON, "Button") \
 MIRYOKU_X(NAV,    "Nav") \
@@ -17,18 +16,39 @@ MIRYOKU_X(MOUSE,  "Mouse") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun")
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(BASE_FROW,   "BaseF") \
+MIRYOKU_X(EXTRA_FROW,  "ExtraF") \
+MIRYOKU_X(TAP_FROW,    "TapF") \
+MIRYOKU_X(BUTTON_FROW, "ButtonF") \
+MIRYOKU_X(NAV_FROW,    "NavF") \
+MIRYOKU_X(MOUSE_FROW,  "MouseF") \
+MIRYOKU_X(MEDIA_FROW,  "MediaF") \
+MIRYOKU_X(NUM_FROW,    "NumF") \
+MIRYOKU_X(SYM_FROW,    "SymF") \
+MIRYOKU_X(FUN_FROW,    "FunF") \
+MIRYOKU_X(GAME,  "Game")
 
 #define U_BASE   0
 #define U_EXTRA  1
-#define U_GAME   2
-#define U_TAP    3
-#define U_BUTTON 4
-#define U_NAV    5
-#define U_MOUSE  6
-#define U_MEDIA  7
-#define U_NUM    8
-#define U_SYM    9
-#define U_FUN    10
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_BASE_FROW   10
+#define U_EXTRA_FROW  11
+#define U_TAP_FROW    12
+#define U_BUTTON_FROW 13
+#define U_NAV_FROW    14
+#define U_MOUSE_FROW  15
+#define U_MEDIA_FROW  16
+#define U_NUM_FROW    17
+#define U_SYM_FROW    18
+#define U_FUN_FROW    19
+#define U_GAME    20
 
 #endif
--- boards/shields/splitPlank60/miryoku/miryoku_behaviors.h
+++ boards/shields/splitPlank60/miryoku/miryoku_behaviors.h
@@ -5,3 +5,4 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+#define U_KR(TAP) &u_lt &key_repeat TAP
