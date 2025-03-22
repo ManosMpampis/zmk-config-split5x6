06d5006d7e6acc22940d362f9985e019f0b17886
ManosMpampis <manosbampis@gmail.com>
2025-03-22 19:10:28 +0200

    add new front row keys

--- boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_alternatives_60.h
+++ boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_alternatives_60.h
@@ -517,5 +517,5 @@ U_NU,              U_NU,              U_NU,              U_NU,              U_NU
 U_NU,              U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,                   U_RDO,                U_PST,             U_CPY,             U_CUT,             U_UND,    U_NU,         \
 U_NU,              &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,                    U_NU,                 &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI, U_NU,         \
 U_NU,              U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,                   U_RDO,                U_PST,             U_CPY,             U_CUT,             U_UND,    U_NU,          \
-U_NU,              U_NU,              U_NU,              U_BTN3,            U_BTN1,            U_BTN2,                  U_BTN2,               U_BTN1,            U_BTN3,            U_NU,              U_NU,     U_NU,
+U_NU,              U_NU,              U_NU,              U_BTN3,            U_BTN1,            U_BTN2,                  U_BTN2,               U_BTN1,            U_BTN3,            U_NU,              U_NU,     U_NU
   
