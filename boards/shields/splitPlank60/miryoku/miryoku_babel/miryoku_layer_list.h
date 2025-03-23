62ee027839374991ba53cb2e7c2490989d801ad8
ManosMpampis <manosbampis@gmail.com>
2025-03-24 00:37:48 +0200

    change the default keys to front row

--- boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_list.h
+++ boards/shields/splitPlank60/miryoku/miryoku_babel/miryoku_layer_list.h
@@ -7,16 +7,6 @@
 #if !defined (MIRYOKU_LAYER_LIST)
 
 #define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(BASE_FROW,   "BaseF") \
 MIRYOKU_X(EXTRA_FROW,  "ExtraF") \
 MIRYOKU_X(TAP_FROW,    "TapF") \
@@ -27,28 +17,38 @@ MIRYOKU_X(MEDIA_FROW,  "MediaF") \
 MIRYOKU_X(NUM_FROW,    "NumF") \
 MIRYOKU_X(SYM_FROW,    "SymF") \
 MIRYOKU_X(FUN_FROW,    "FunF") \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(GAME,  "Game")
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
-#define U_BASE_FROW   10
-#define U_EXTRA_FROW  11
-#define U_TAP_FROW    12
-#define U_BUTTON_FROW 13
-#define U_NAV_FROW    14
-#define U_MOUSE_FROW  15
-#define U_MEDIA_FROW  16
-#define U_NUM_FROW    17
-#define U_SYM_FROW    18
-#define U_FUN_FROW    19
+#define U_BASE_FROW   0
+#define U_EXTRA_FROW  1
+#define U_TAP_FROW    2
+#define U_BUTTON_FROW 3
+#define U_NAV_FROW    4
+#define U_MOUSE_FROW  5
+#define U_MEDIA_FROW  6
+#define U_NUM_FROW    7
+#define U_SYM_FROW    8
+#define U_FUN_FROW    9
+#define U_BASE   10
+#define U_EXTRA  11
+#define U_TAP    12
+#define U_BUTTON 13
+#define U_NAV    14
+#define U_MOUSE  15
+#define U_MEDIA  16
+#define U_NUM    17
+#define U_SYM    18
+#define U_FUN    19
 #define U_GAME    20
 
 #endif
