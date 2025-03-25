245687bf1e3b40096d64da5ad217314cfa7fcdbe
ManosMpampis <manosbampis@gmail.com>
2025-03-25 21:59:00 +0200

    fix scroll and mouse speed, add smooth scroll

--- boards/shields/splitPlank60/miryoku/miryoku_kludge_mousekeyspr.h
+++ boards/shields/splitPlank60/miryoku/miryoku_kludge_mousekeyspr.h
@@ -3,17 +3,19 @@
 
 #pragma once
 
-#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1250
-#define ZMK_MOUSE_DEFAULT_SCRL_VAL 100
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 900
+#define ZMK_MOUSE_DEFAULT_SCRL_VAL 700
 
-#include <dt-bindings/zmk/pointing.h>
+// #define ZMK_MOUSE_DEFAULT_SCRL_VAL 70
 
 #define U_MOUSE_MOVE_EXPONENT 1
-#define U_MOUSE_MOVE_TIME 1500
+#define U_MOUSE_MOVE_TIME 500
 #define U_MOUSE_MOVE_DELAY 0
 #define U_MOUSE_SCROLL_EXPONENT 1
-#define U_MOUSE_SCROLL_TIME 5000
+#define U_MOUSE_SCROLL_TIME 1000
 #define U_MOUSE_SCROLL_DELAY 0
+
+#include <dt-bindings/zmk/pointing.h>
 
 #define U_BTN1 &mkp MB1
 #define U_BTN2 &mkp MB2
